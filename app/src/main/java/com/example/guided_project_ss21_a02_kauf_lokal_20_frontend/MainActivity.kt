5f922a85c34d7ce0dec854c55f7579bb3813d52c
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-06-03 13:11:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
