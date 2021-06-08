0a06f5761c0173da33ded9d4727fb6c1a1a6d25d
Deniz-Uzun <deniz.uzun@smail.th-koeln.de>
2021-06-08 17:40:04 +0200

    Project initially implemented. Created Fragments and Bottom Navigationbar.

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/MainActivity.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Speichern der BottomNavigation in Variable
+        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        //Finden des NavControllers des NavHosts
+        val navController = findNavController(navView)
+        //NavBar zur Nutzung des NavControllers ausstatten
+        navView.setupWithNavController(navController)
+
     }
 }
