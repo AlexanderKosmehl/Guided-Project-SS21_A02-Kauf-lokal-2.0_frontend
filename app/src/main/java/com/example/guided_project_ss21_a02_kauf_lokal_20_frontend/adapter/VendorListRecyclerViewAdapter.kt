8a9331f5bc9f336a8b16b4c2a8e12d55da137ae6
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-06-15 23:01:45 +0200

    Adapter now properly handles dummy vendor data

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/adapter/VendorListRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/adapter/VendorListRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.adapter
 
+import android.graphics.Color
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.TextView
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.R
 
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.dummy.DummyContent.DummyItem
+import kotlin.random.Random
 
 /**
  * [RecyclerView.Adapter] that can display a [DummyItem].
@@ -18,19 +20,22 @@ class VendorListRecyclerViewAdapter(
     private val values: List<DummyItem>
 ) : RecyclerView.Adapter<VendorListRecyclerViewAdapter.ViewHolder>() {
 
+    val colors = listOf(R.color.teal_200, R.color.teal_700, R.color.purple_200, R.color.purple_500, R.color.purple_700, R.color.star_fill)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.fragment_vendor_list, parent, false)
+            .inflate(R.layout.fragment_vendor_list_item, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
         holder.titleView.text = item.title
-        holder.ratingBar.numStars = item.rating
+        holder.titleView.setBackgroundResource(colors.random())
+        holder.ratingBar.rating = item.rating.toFloat()
         holder.categoryView.text = item.vendorCategory
         holder.isOpenView.text = if (item.isOpen) "Geöffnet" else "Geschlossen"
-        holder.distanceView.text = item.distance.toString()
+        holder.distanceView.text = item.distance.toString() + "m"
     }
 
     override fun getItemCount(): Int = values.size
