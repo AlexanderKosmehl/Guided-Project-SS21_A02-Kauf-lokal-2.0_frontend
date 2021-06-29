6b0b5466080f9b582691043ba0e5dfc9660af49a
Daniel Alile <daniel.alile@smail.th-koeln.de>
2021-06-29 22:45:36 +0200

    first attempt to process queried backend-data

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/adapter/NewsfeedRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/adapter/NewsfeedRecyclerViewAdapter.kt
@@ -1,20 +1,24 @@
 package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.adapter
 
+import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.R
 
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.dummy.DummyContent.DummyItem
+import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.fragments.DetailEvent
+import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model.Coupon
 
 /**
  * [RecyclerView.Adapter] that can display a [DummyItem].
  * TODO: Replace the implementation with code for your data type.
  */
 class NewsfeedRecyclerViewAdapter(
-    private val values: List<DummyItem>
+    private val coupons: List<Coupon>
 ) : RecyclerView.Adapter<NewsfeedRecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -24,18 +28,30 @@ class NewsfeedRecyclerViewAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = values[position]
-        holder.idView.text = item.id
+        val coupon = coupons[position]
+        holder.titleView.text = coupon.id
     }
 
-    override fun getItemCount(): Int = values.size
+    override fun getItemCount(): Int = coupons.size
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val idView: TextView = view.findViewById(R.id.vendor_title)
-        val contentView: TextView = view.findViewById(R.id.content)
+        var titleView: TextView
 
-        override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
+        init {
+            titleView = view.findViewById(R.id.event_title)
+
+            itemView.setOnClickListener {
+                var position: Int = getAdapterPosition()
+                val context = itemView.context
+                val intent = Intent(context, DetailEvent::class.java).apply {
+                    putExtra("NUMBER", position)
+                    /*putExtra("CODE", itemKode.text)
+                    putExtra("CATEGORY", itemKategori.text)
+                    putExtra("CONTENT", itemIsi.text)*/
+                }
+                context.startActivity(intent)
+            }
         }
+
     }
 }
--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/fragments/NewsfeedFragment.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/fragments/NewsfeedFragment.kt
@@ -2,62 +2,30 @@ package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.adapter.NewsfeedRecyclerViewAdapter
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.R
-import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.dummy.DummyContent
+import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.service.PollingService
 
 /**
- * A fragment representing a list of Items.
+ * A fragment representing a list of Items for the newsfeed.
  */
 class NewsfeedFragment : Fragment() {
 
-    private var columnCount = 1
+    private var adapter: RecyclerView.Adapter<NewsfeedRecyclerViewAdapter.ViewHolder>? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
-    }
+    private var pollingService = PollingService(requireContext())
+    private val coupons = pollingService.pollCoupons()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_newsfeed_list, container, false)
+        ): View? {
+        adapter = NewsfeedRecyclerViewAdapter(coupons)
 
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = NewsfeedRecyclerViewAdapter(DummyContent.ITEMS)
-            }
-        }
-        return view
-    }
-
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            NewsfeedFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
+        return inflater.inflate(R.layout.fragment_newsfeed_list, container, false)
     }
 }
