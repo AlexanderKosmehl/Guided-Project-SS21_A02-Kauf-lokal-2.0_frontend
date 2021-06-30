2700e4ee2498ce1f851a1651da1ee424610db0af
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-06-30 14:02:16 +0200

    VendorListAdapter now handles vendors instead of DummyItems

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/adapter/VendorListRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/adapter/VendorListRecyclerViewAdapter.kt
@@ -11,15 +11,18 @@ import android.widget.RatingBar
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.Model.Vendor
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.R
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.dummy.DummyContent.DummyItem
+import kotlin.random.Random
 
 /**
  * [RecyclerView.Adapter] that can display a [DummyItem].
  * TODO: Replace the implementation with code for your data type.
  */
 class VendorListRecyclerViewAdapter(
-    private val values: List<DummyItem>
+    private var vendors: List<Vendor>
+
 ) : RecyclerView.Adapter<VendorListRecyclerViewAdapter.ViewHolder>() {
 
     val colors = listOf(
@@ -31,6 +34,12 @@ class VendorListRecyclerViewAdapter(
         R.color.star_fill
     )
 
+    // Automatically displays data changes
+    fun setValues(vendors: List<Vendor>) {
+        this.vendors = vendors
+        this.notifyDataSetChanged()
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.fragment_vendor_list_item, parent, false)
@@ -38,24 +47,44 @@ class VendorListRecyclerViewAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = values[position]
-        //holder.titleView.text = item.title
+        val vendor = vendors[position]
+
+        // TODO handle profilePicture URL(?) once backend implements it
+        if (vendor.profilePicture != null) {
+            // Handle picture
+        } else {
+            holder.titleView.text = vendor.name
+        }
+
+        // TODO Change when backend adds value
         holder.headerLayout.setBackgroundResource(colors.random())
-        holder.ratingBar.rating = item.rating.toFloat()
-        holder.categoryView.text = item.vendorCategory
-        holder.isOpenView.text = if (item.isOpen) "Geöffnet" else "Geschlossen"
+
+        // TODO MerchantScore is currently an integer
+        holder.ratingBar.rating = vendor.merchantScore.toFloat()
+
+        // TODO Change when backend adds value
+        holder.categoryView.text = "No Category"
+
+        // TODO Calculate value once backend adds support
+        holder.isOpenView.text = if (listOf(true, false).random()) "Geöffnet" else "Geschlossen"
+
         holder.isOpenView.setTextColor(
             holder.isOpenView.context.resources.getColor(
-                if (item.isOpen) R.color.open_color else R.color.close_color,
+                if (holder.isOpenView.text == "Geöffnet") R.color.open_color else R.color.close_color,
                 null
             )
         )
-        holder.distanceView.text = item.distance.toString() + "m"
+
+        // TODO Calculate value once backend adds support
+        holder.distanceView.text = (Random.nextInt(20) * 50).toString() + "m"
+
+        // TODO Change when backend adds value
         holder.isFavoView.setImageResource(
-            if (item.isFavo) R.drawable.ic_baseline_favorite_24
+            if (listOf(true, false).random()) R.drawable.ic_baseline_favorite_24
             else R.drawable.ic_baseline_favorite_border_24
         )
 
+        // Display vendor details on tap
         holder.cardView.setOnClickListener {
             if (holder.hiddenView.visibility == View.VISIBLE) {
                 TransitionManager.beginDelayedTransition(holder.cardView, AutoTransition())
@@ -67,7 +96,7 @@ class VendorListRecyclerViewAdapter(
         }
     }
 
-    override fun getItemCount(): Int = values.size
+    override fun getItemCount(): Int = vendors.size
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val titleView: TextView = view.findViewById(R.id.vendor_title)
@@ -77,9 +106,9 @@ class VendorListRecyclerViewAdapter(
         val isOpenView: TextView = view.findViewById(R.id.vendor_is_open)
         val distanceView: TextView = view.findViewById(R.id.vendor_distance)
         val isFavoView: ImageView = view.findViewById(R.id.vendor_is_favo)
+        val logoView: ImageView = view.findViewById(R.id.vendor_logo)
 
         val cardView: CardView = view.findViewById(R.id.vendorCardView)
         val hiddenView: ConstraintLayout = view.findViewById(R.id.hiddenView)
-
     }
 }
