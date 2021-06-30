3dbd4303c448e0f802e7e409e613b8e056dde541
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-06-30 14:00:48 +0200

    Added first version of backend communication

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/fragments/VendorListFragment.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/fragments/VendorListFragment.kt
@@ -18,9 +18,11 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.Model.Vendor
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.adapter.VendorListRecyclerViewAdapter
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.R
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.dummy.DummyContent
+import com.google.gson.Gson
 
 /**
  * A fragment representing a list of Items.
@@ -36,9 +38,40 @@ class VendorListFragment : Fragment() {
         // Set LayoutManager and Adapter
         with(view) {
             layoutManager = LinearLayoutManager(context)
-            adapter = VendorListRecyclerViewAdapter(DummyContent.ITEMS)
+            adapter = VendorListRecyclerViewAdapter(listOf<Vendor>())
         }
 
+        // Handles backend communication
+        addVendorsToAdapter(view)
+
         return view
+    }
+
+    fun addVendorsToAdapter(view: RecyclerView) {
+        val context = view.context
+        val adapter = view.adapter as VendorListRecyclerViewAdapter
+
+        val gson = Gson()
+
+        val url = "http://10.0.2.2:8080/merchant"
+        val queue = Volley.newRequestQueue(context)
+        val vendors = mutableListOf<Vendor>()
+
+        val request = JsonArrayRequest(Request.Method.GET, url, null,
+            { response ->
+                // JSONArray does not support iterable which means this has to be a regular for loop
+                for (i in 0 until response.length()) {
+                    val vendor = gson.fromJson(response.getJSONObject(i).toString(), Vendor::class.java)
+                    vendors.add(vendor)
+                }
+                // TODO Maybe add vendors one by one for smoother vendor view?
+                adapter.setValues(vendors)
+            },
+            { error ->
+                // TODO Add meaningful error handling
+                Log.e("Response", error.message ?: "Keine Fehlermeldung vorhanden")
+            }
+            )
+        queue.add(request)
     }
 }
