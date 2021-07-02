c36eb6e4c4db5666ff5d141d5d7432f68df25e77
KayRuck <kay.nicole.ruck@web.de>
2021-07-02 12:01:45 +0200

    Revert "Optimized Imports"
    This reverts commit f488df35

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/Model/Address.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/Model/Address.kt
@@ -1,3 +1,3 @@
-package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model
+package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.Model
 
 data class Address(var street: String, var houseNr: String, var place: String, var zipCode: String, var country: String)
