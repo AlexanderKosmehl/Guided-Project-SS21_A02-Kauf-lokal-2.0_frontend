6a3dbad614b0abe387c9e934a60d7f0cdab6a175
KayRuck <kay.nicole.ruck@web.de>
2021-07-02 12:23:10 +0200

    fixup! from optimize imports

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/adapter/VendorListRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/adapter/VendorListRecyclerViewAdapter.kt
@@ -1,8 +1,8 @@
 package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.adapter
 
+import android.annotation.SuppressLint
 import android.transition.AutoTransition
 import android.transition.TransitionManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +12,8 @@ import android.widget.RatingBar
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.Model.OpeningTime
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.Model.Vendor
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.R
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.dummy.DummyContent.DummyItem
@@ -21,18 +23,27 @@ import kotlin.random.Random
  * [RecyclerView.Adapter] that can display a [DummyItem].
  * TODO: Replace the implementation with code for your data type.
  */
+
+// TODO fix the warning later
+@SuppressLint("SetTextI18n")
 class VendorListRecyclerViewAdapter(
     private var vendors: List<Vendor>
 
 ) : RecyclerView.Adapter<VendorListRecyclerViewAdapter.ViewHolder>() {
 
     private val colors = listOf(
-        R.color.teal_200,
+        R.color.risse_green,
+        R.color.open_color,
+        R.color.tchibo_blue,
         R.color.teal_700,
+        R.color.teal_200,
         R.color.purple_200,
         R.color.purple_500,
         R.color.purple_700,
-        R.color.star_fill
+        R.color.star_fill,
+        R.color.close_color,
+        R.color.button_ripple_red,
+        R.color.saturn_orange
     )
 
     // Automatically displays data changes
@@ -49,6 +60,7 @@ class VendorListRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val color = colors.random()
+        val isOpen = listOf(true, false).random()
 
         val vendor = vendors[position]
 
@@ -56,22 +68,23 @@ class VendorListRecyclerViewAdapter(
         if (vendor.profilePicture != null) {
             // Handle picture
         } else {
-            holder.titleView.text = vendor.name
+            holder.logoView.visibility = View.GONE
+            if (vendor.name != "Forum Gummersbach") // TODO remove fix when names in BE is fixed
+                holder.titleView.text = vendor.name.replace("Gummersbach", "", false)
+            else holder.titleView.text = vendor.name
         }
 
         // TODO Change when backend adds value
         holder.headerLayout.setBackgroundResource(color)
-        holder.couponsButton.setBackgroundColor(color)
-        holder.routeButton.setBackgroundColor(color)
 
         // TODO MerchantScore is currently an integer
         holder.ratingBar.rating = vendor.merchantScore.toFloat()
 
         // TODO Change when backend adds value
-        holder.categoryView.text = "No Category"
+        holder.categoryView.text = "Keine Kategorie"
 
         // TODO Calculate value once backend adds support
-        holder.isOpenView.text = if (listOf(true, false).random()) "Geöffnet" else "Geschlossen"
+        holder.isOpenView.text = if (isOpen) "Geöffnet" else "Geschlossen"
 
         holder.isOpenView.setTextColor(
             holder.isOpenView.context.resources.getColor(
@@ -91,40 +104,82 @@ class VendorListRecyclerViewAdapter(
 
         // Display vendor details on tap
         holder.cardView.setOnClickListener {
-            if (holder.hiddenView.visibility == View.VISIBLE) {
+            if (holder.unfoldedView.visibility == View.VISIBLE) {
                 holder.bodyLayout.visibility = View.VISIBLE
-                // TransitionManager.beginDelayedTransition(holder.cardView, AutoTransition())
-                holder.hiddenView.visibility = View.GONE
+                holder.unfoldedView.visibility = View.GONE
             } else {
                 holder.bodyLayout.visibility = View.GONE
+                holder.unfoldedView.visibility = View.VISIBLE
+                displayHiddenItems(holder, vendor, color, isOpen)
                 TransitionManager.beginDelayedTransition(holder.cardView, AutoTransition())
-                holder.hiddenView.visibility = View.VISIBLE
-                holder.categoryViewHidden.text = "Keine Kategorie"
-                holder.ratingBarViewHidden.rating = vendor.merchantScore.toFloat()
             }
         }
     }
 
+    // TODO something useful?
+    private fun isVendorOpen(openingTime: OpeningTime): Boolean {
+        return listOf(true, false).random()
+    }
+
     override fun getItemCount(): Int = vendors.size
 
+    // TODO Change when backend adds usable values
+    private fun displayHiddenItems(
+        holder: ViewHolder,
+        vendor: Vendor,
+        color: Int,
+        isOpen: Boolean
+    ) {
+        val colorRes = holder.couponsButton.context.resources.getColor(color, null)
+
+        holder.categoryUnfoldView.text = "Keine Kategorie"
+        holder.websiteUnfoldView.text = "Keine Website"
+        holder.addressUnfoldView.text = "${vendor.address.street} ${vendor.address.houseNr}"
+        holder.ratingCountUnfoldView.text = "(${Random.nextInt(1, 1001)})"
+        holder.ratingBarUnfold.rating = vendor.merchantScore.toFloat()
+
+        holder.isOpenUnfoldView.text = if (isOpen) "Geöffnet" else "Geschlossen"
+        holder.isOpenUnfoldView.setTextColor(
+            holder.isOpenUnfoldView.context.resources.getColor(
+                if (holder.isOpenUnfoldView.text == "Geöffnet")
+                    R.color.open_color else R.color.close_color, null
+            )
+        )
+
+        holder.couponsButton.setBackgroundColor(colorRes)
+        holder.routeButton.setBackgroundColor(colorRes)
+        holder.websiteUnfoldImage.setColorFilter(colorRes)
+        holder.addressUnfoldImage.setColorFilter(colorRes)
+    }
+
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val titleView: TextView = view.findViewById(R.id.vendor_title)
+        //--- both
         val headerLayout: ConstraintLayout = view.findViewById(R.id.headerLayout)
+        val cardView: CardView = view.findViewById(R.id.vendorCardView)
+        val titleView: TextView = view.findViewById(R.id.vendor_title)
+        val isFavoView: ImageView = view.findViewById(R.id.vendor_is_favo)
+        val logoView: ImageView = view.findViewById(R.id.vendor_logo)
+
+        //--- collapsed
+        val bodyLayout: ConstraintLayout = view.findViewById(R.id.simpleBodyLayout)
         val ratingBar: RatingBar = view.findViewById(R.id.vendor_rating_bar)
         val categoryView: TextView = view.findViewById(R.id.vendor_category)
         val isOpenView: TextView = view.findViewById(R.id.vendor_is_open)
         val distanceView: TextView = view.findViewById(R.id.vendor_distance)
-        val isFavoView: ImageView = view.findViewById(R.id.vendor_is_favo)
-        val logoView: ImageView = view.findViewById(R.id.vendor_logo)
-        val couponsButton : Button = view.findViewById(R.id.coupons_button)
-        val routeButton : Button = view.findViewById(R.id.route_button)
 
-        val bodyLayout :ConstraintLayout = view.findViewById(R.id.simpleBodyLayout)
+        //--- unfolded
+        val unfoldedView: ConstraintLayout = view.findViewById(R.id.unfoldedView)
+        val couponsButton: Button = view.findViewById(R.id.vendor_coupons_button)
+        val routeButton: Button = view.findViewById(R.id.vendor_route_button)
+        val ratingBarUnfold: RatingBar = view.findViewById(R.id.vendor_rating_bar_unfold)
+        val categoryUnfoldView: TextView = view.findViewById(R.id.vendor_category_unfold)
+        val websiteUnfoldView: TextView = view.findViewById(R.id.vendor_website_link)
+        val addressUnfoldView: TextView = view.findViewById(R.id.vendor_address_content)
+        val isOpenUnfoldView: TextView = view.findViewById(R.id.vendor_is_open_unfold)
+        val ratingCountUnfoldView: TextView = view.findViewById(R.id.vendor_rating_count)
+        val websiteUnfoldImage: ImageView = view.findViewById(R.id.vendor_web_icon)
+        val addressUnfoldImage: ImageView = view.findViewById(R.id.vendor_address_icon)
 
-        val ratingBarViewHidden : RatingBar = view.findViewById(R.id.vendor_rating_bar2)
-        val categoryViewHidden : TextView = view.findViewById(R.id.vendor_category2)
-
-        val cardView: CardView = view.findViewById(R.id.vendorCardView)
-        val hiddenView: ConstraintLayout = view.findViewById(R.id.hiddenView)
     }
 }
+
