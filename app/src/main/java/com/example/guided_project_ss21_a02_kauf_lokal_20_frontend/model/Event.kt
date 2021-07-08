9a1c665bc97c85f78e2ee0677fdc16ef83109f72
Daniel Alile <daniel.alile@smail.th-koeln.de>
2021-07-09 00:22:43 +0200

    added new data classes for newsfeed

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Event.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Event.kt
@@ -1,7 +1,12 @@
 package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model
 
-interface Event {
-    // TODO: tbd, Backend!
-    var id: String
-    var timestamp: String
-}
+import java.time.LocalDateTime
+import java.util.*
+
+data class Event (
+    var id: UUID,
+    var eventTypes: EventTypes,
+    var refId: UUID,
+    var vendorId: UUID,
+    var created: String
+)
---
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Message.kt
@@ -0,0 +1,9 @@
+package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model
+
+import java.util.*
+
+data class Message(
+    var id: UUID,
+    var message: String,
+    var created: Date
+)
