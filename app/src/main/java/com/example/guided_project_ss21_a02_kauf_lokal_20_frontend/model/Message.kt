3bdd191d4e770d2a4025d7e35c40146f45ce6d33
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-07-11 21:52:59 +0200

    Added Message class

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Message.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Message.kt
@@ -1,13 +1,12 @@
 package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
 import java.util.*
 
-@kotlinx.parcelize.Parcelize
 data class Message(
-    var id: UUID,
+    var id: String,
+    var title: String,
     var message: String,
+    var imageURL: String,
     var created: Date
-): Parcelable
+
+)
