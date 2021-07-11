63c14fd4ea47059192232b129d7155572263a386
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-07-11 22:29:38 +0200

    Fixed url

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/fragments/VendorListFragment.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/fragments/VendorListFragment.kt
@@ -45,7 +45,8 @@ class VendorListFragment : Fragment() {
 
         val gson = Gson()
 
-        val url = "http://10.0.2.2:8080/merchant"
+        // TODO Add resource file for urls
+        val url = "http://10.0.2.2:8080/vendor"
         val queue = Volley.newRequestQueue(context)
         val vendors = mutableListOf<Vendor>()
 
@@ -56,7 +57,6 @@ class VendorListFragment : Fragment() {
                     val vendor = gson.fromJson(response.getJSONObject(i).toString(), Vendor::class.java)
                     vendors.add(vendor)
                 }
-                // TODO Maybe add vendors one by one for smoother vendor view?
                 adapter.setValues(vendors)
             },
             { error ->
