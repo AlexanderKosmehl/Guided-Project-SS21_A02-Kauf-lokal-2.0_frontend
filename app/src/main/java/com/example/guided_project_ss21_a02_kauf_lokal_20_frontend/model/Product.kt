6acbac4d4b9377872c158c15ba868413abaf2c9f
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-07-11 21:53:08 +0200

    Added Product class

---
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Product.kt
@@ -0,0 +1,8 @@
+package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model
+
+data class Product(
+    var id: String,
+    var name: String,
+    var description: String,
+    var price: Double
+)
