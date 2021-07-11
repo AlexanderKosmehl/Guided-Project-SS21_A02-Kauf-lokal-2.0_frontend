5fa4081ef287f55d27c80ede3659e8d68d61873c
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-07-11 22:29:17 +0200

    Attempted to handle null values from backend

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Vendor.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Vendor.kt
@@ -2,23 +2,24 @@ package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model
 
 import java.util.*
 
+// TODO Re-check nullables
 data class Vendor(
     var id: UUID,
     var name: String,
-    var offerAmount: Int,
-    var vendorScore: Int,
+    var offerAmount: Int?,
+    var vendorScore: Int?,
     var logo: String,
     var websiteURL: String,
-    var color: String,
-    var category: VendorCategory,
+    var color: String?,
+    var category: VendorCategory?,
     var address: Address,
     var emailAddress: String,
     var company: String,
-    var openingTime: OpeningTime,
+    var openingTime: OpeningTime?,
     var detailImages: Set<String?>,
-    var coupons: Set<Coupon?>,
+    // var coupons: Set<Coupon?>,   // TODO Fix coupon once they are finished
     var ratings: Set<Rating?>,
     var products: Set<Product?>,
-    var messages: Set<Message?>,
-    var events: Set<Event?>
+    // var messages: Set<Message?>, // TODO Fix messages once they are finished
+    // var events: Set<Event?>      // TODO Fix events once they are finished
 )
