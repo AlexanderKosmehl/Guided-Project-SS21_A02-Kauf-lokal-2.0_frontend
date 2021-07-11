e6edf0ad50dcc85148b0f726b2854afdd2fb0ab3
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-07-11 21:52:59 +0200

    Added Message class

---
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Message.kt
@@ -0,0 +1,12 @@
+package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model
+
+import java.util.*
+
+data class Message(
+    var id: String,
+    var title: String,
+    var message: String,
+    var imageURL: String,
+    var created: Date
+
+)
