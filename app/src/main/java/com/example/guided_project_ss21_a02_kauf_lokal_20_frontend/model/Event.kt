594c5424e56c60aac8ed7052ee9d0bb25237a715
Daniel Alile <daniel.alile@smail.th-koeln.de>
2021-07-14 01:00:38 +0200

    added Parcelling (needed for passing data with navgraph)

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Event.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Event.kt
@@ -1,12 +1,15 @@
 package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.time.LocalDateTime
 import java.util.*
 
+@Parcelize
 data class Event (
     var id: UUID,
     var eventTypes: EventTypes,
     var refId: UUID,
     var vendorId: UUID,
-    var created: String
-)
+    var created: Date
+): Parcelable
--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Message.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Message.kt
@@ -1,9 +1,13 @@
 package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 import java.util.*
 
+@kotlinx.parcelize.Parcelize
 data class Message(
     var id: UUID,
     var message: String,
     var created: Date
-)
+): Parcelable
