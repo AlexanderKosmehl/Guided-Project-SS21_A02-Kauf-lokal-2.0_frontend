bb9ca214d5f87b28f9c7a9c0fe87cfffc709413b
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-07-19 09:27:50 +0200

    Changed to match layout

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/adapter/CouponListRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/adapter/CouponListRecyclerViewAdapter.kt
@@ -1,12 +1,17 @@
 package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.R
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model.Coupon
+import java.time.LocalDate
+import java.util.*
 
 class CouponListRecyclerViewAdapter(
     private var coupons: List<Coupon>
@@ -24,13 +29,39 @@ class CouponListRecyclerViewAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // holder.textView.text = "Demo"
+        // Coupon Color
+        // TODO Use vendor color some something similar
+        holder.couponHeader.setBackgroundResource(R.color.saturn_orange)
+
+        // Coupon Logo
+        // TODO Handle image
+
+        // Coupon Text
+        holder.couponText.text = coupons[position].description ?: "50 % Rabatt"
+
+        // Coupon Date
+        val createdDate = coupons[position].created
+        val expiryDate = coupons[position].expiryDate
+
+        val monthStrings = listOf("Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August","September", "Oktober", "November", "Dezember")
+
+        val calendar = GregorianCalendar()
+        calendar.time = createdDate
+        val createdString = "${calendar.get(Calendar.DAY_OF_MONTH)} ${monthStrings[calendar.get(Calendar.MONTH)]}"
+        calendar.time = expiryDate
+        val expiryString = "${calendar.get(Calendar.DAY_OF_MONTH)} ${monthStrings[calendar.get(Calendar.MONTH)]}"
+
+        holder.couponDate.text = "$createdString - $expiryString" ?: "Ohne Ablaufdatum"
     }
 
     override fun getItemCount(): Int = coupons.size
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.percentageText)
+        val couponHeader: ConstraintLayout = view.findViewById(R.id.couponHeaderLayout)
+        val couponLogo: ImageView = view.findViewById(R.id.couponLogoView)
+        val couponFavorite: ImageView = view.findViewById(R.id.couponIsFavo)
+        val couponText: TextView = view.findViewById(R.id.couponText)
+        val couponDate: TextView = view.findViewById(R.id.couponDate)
     }
 
 
