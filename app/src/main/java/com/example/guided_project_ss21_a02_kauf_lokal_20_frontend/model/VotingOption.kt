8c20917a6fdda4ebb587949b66df81ba39e39248
Daniel Alile <daniel.alile@smail.th-koeln.de>
2021-07-19 18:18:14 +0200

    implemented data classes for polls

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/VotingOption.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/VotingOption.kt
@@ -3,13 +3,10 @@ package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model
 import android.os.Parcelable
 import java.util.*
 
-// TODO
 @kotlinx.parcelize.Parcelize
-data class Poll(
+data class VotingOption(
     var id: UUID,
-    var title: String,
-    var authorName: String,
-    var totalAmountVoters: Int,
-    var authorProfilePicture: String,
-    var options: List<VotingOption>,
+    var name: String,
+    var amountVoters: Int,
 ): Parcelable
+
