9044cab8888b1a7e5d2ce54c3a77b446fc4cd8fc
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-07-23 23:28:43 +0200

    Made Coupon Parcelable

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Coupon.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/model/Coupon.kt
@@ -1,8 +1,11 @@
 package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.util.*
 
 // TODO: Backend
+@Parcelize
 data class Coupon(
     var id: UUID,
     var name: String,
@@ -11,4 +14,4 @@ data class Coupon(
     var expiryDate: Date,
     var value: Float,
     var created: Date
-)
+): Parcelable
