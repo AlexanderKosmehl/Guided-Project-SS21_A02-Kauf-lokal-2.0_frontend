9e9620be784eb8002306e801e0fdc8f7b2b86c53
Daniel Alile <daniel.alile@smail.th-koeln.de>
2022-02-06 21:13:44 +0100

    added static map marker

--- app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/fragments/VendorMapFragment.kt
+++ app/src/main/java/com/example/guided_project_ss21_a02_kauf_lokal_20_frontend/fragments/VendorMapFragment.kt
@@ -1,27 +1,52 @@
 package com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.fragments
 
+import android.location.Geocoder
+import androidx.fragment.app.Fragment
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.R
-import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.adapter.VendorListRecyclerViewAdapter
 import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.model.Vendor
-import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.utilities.TitleTexts
-import com.example.guided_project_ss21_a02_kauf_lokal_20_frontend.viewModel.VendorListViewModel
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class VendorMapFragment : Fragment() {
 
+    private var vendors = listOf<Vendor>()
+
+
+    private val callback = OnMapReadyCallback { googleMap ->
+
+        val location = LatLng(51.0266668, 7.569283)
+
+        googleMap.addMarker(MarkerOptions().position(location).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(18F))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
+
+
+    }
+
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_vendor_map, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+
 }
